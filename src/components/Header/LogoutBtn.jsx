958f3c07e747470f4c3977f4e120e86a7927eadc
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-10 11:35:01 +0530

    small changes in logoutbtn.jsx file

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -4,12 +4,18 @@ import { logout } from "../../store/authSlice";
 
 function LogoutBtn() {
   const dispatch = useDispatch();
+
   const logoutHandler = () => {
-    dispatch(logout());
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
   };
 
   return (
-    <div className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+    <div
+      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
+    >
       Logout
     </div>
   );
