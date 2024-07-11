0d020657150a2ee536c41567e8c232910f749457
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-11 11:19:52 +0530

    small change in header.jsx file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -48,8 +48,8 @@ function Header() {
               item.active ? (
                 <li key={item.name}>
                   <button
-                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
                     onClick={navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
                   >
                     {item.name}
                   </button>
